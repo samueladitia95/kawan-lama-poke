c2bcfb632d46035796a4dba5f968abe1aa4f1eb0
Samuel Aditia <samueladitia95@gmail.com>
2022-07-28 22:15:10 +0700

    Add tailwind color variable and container

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ["Poppins"],
+      },
+      colors: {
+        bLightPrimary: "#ffffff",
+        bLightSecondary: "#f0f1f2",
+        tLightPrimary: "#242424",
+        bDarkPrimary: "#121318",
+        bDarkSecondary: "#212328",
+        tDarkPrimary: "#dbdbdb",
+        accent1: "#ff6740",
+      },
+    },
+    container: {
+      center: true,
+      padding: "1.5rem",
+    },
   },
   plugins: [],
+  darkMode: "class",
 };
