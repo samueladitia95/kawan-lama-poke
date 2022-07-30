e94a3540f4aae5e90b944592e0239a61c21f9c69
Samuel Aditia <samueladitia95@gmail.com>
2022-07-30 21:03:40 +0700

    Move random function to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Head from "next/head";
 import { GetServerSideProps } from "next";
 import { fetcher } from "../utils/fetcher";
 import Card from "../components/Card";
 import { Name } from "../type";
 
-const Home = ({ randomPokemonUrls }: { randomPokemonUrls: Name[] }) => {
+const Home = ({ pokemons }: { pokemons: Name[] }) => {
   const [isMax, setIsMax] = useState<boolean>(false);
-
+  const [randomPokemons, setRandomPokemon] = useState<Name[]>([]);
+  useEffect(() => {
+    const randomPokemonUrls: Name[] = Array(5)
+      .fill("")
+      .map(() => {
+        return pokemons[Math.floor(Math.random() * 1154)];
+      });
+    setRandomPokemon(randomPokemonUrls);
+  }, [pokemons]);
   return (
     <Fragment>
       <Head>
@@ -23,7 +31,7 @@ const Home = ({ randomPokemonUrls }: { randomPokemonUrls: Name[] }) => {
           Catch Them All
         </p>
         <div className="flex flex-col items-center lg:flex-row lg:flex-wrap lg:gap-4 lg:justify-center">
-          {randomPokemonUrls.map(({ url }, index: number) => {
+          {randomPokemons.map(({ url }, index: number) => {
             return (
               <Card key={index} url={url} isMax={isMax} setIsMax={setIsMax} />
             );
@@ -44,14 +52,9 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const data = await fetcher(`https://pokeapi.co/api/v2/pokemon?limit=1154`);
 
   const pokemons: Name[] = data.results;
-  const randomPokemonUrls: Name[] = Array(5)
-    .fill("")
-    .map(() => {
-      return pokemons[Math.floor(Math.random() * 1154)];
-    });
   return {
     props: {
-      randomPokemonUrls,
+      pokemons,
     },
   };
 };
