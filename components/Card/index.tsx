ac686966b5731e10b108b9a055ac548f4d358639
Samuel Aditia <samueladitia95@gmail.com>
2022-07-30 11:16:37 +0700

    Add Loading to main page card

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,11 +1,17 @@
 import { useCard } from "./useCard";
+import Loading from "../Loading";
 
 export default function Card({ url }: { url: string }) {
-  const { pokemon, isLoading, isError } = useCard(url);
-  console.log(pokemon ? pokemon.id : "", isLoading, isError);
+  const { isLoading, pokemon } = useCard(url);
+  console.log(pokemon);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <div>
-      <h1 className="dark:text-tDarkPrimary">This is a card</h1>
+      <h1 className="dark:text-tDarkPrimary">{pokemon.name}</h1>
     </div>
   );
 }
